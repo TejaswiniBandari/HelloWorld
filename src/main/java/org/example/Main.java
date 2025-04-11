0b9fa2084c823f63c208c339273b634d20857b72
bandaritejaswini <tejaswinibandari9@gmail.com>
2025-04-11 18:49:45 +0530

    fifth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
-         System.out.println("Hello, Bangalore");
+        System.out.println("Hello, Bangalore");
         System.out.println("Hello, Vijayawada");
-          System.out.println("Hello, Hyderabad");
-        
-        
+        System.out.println("Hello, Hyderabad");
+        System.out.println("Hello, Ghantashala");
     }
 }
